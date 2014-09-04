a2ca9766ca83f5559f8c9f8ca2e87a9b07f8fe47
megrez <lujiajing1126@gmail.com>
2014-09-04 12:35:08 +0800

    add iphone 4 5

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,25 @@
 $(function(){
 	'use strict';
 	var ua = window.navigator.userAgent,
-		iPhoneUA = /iPhone/i;
-	console.log(""+ua);
+		iPhoneUA = /iPhone/i,
+		isiPhone = iPhoneUA.test(ua),
+		inMicroMessenger = /MicroMessenger/i,
+		isMicroMessenger = inMicroMessenger.test(ua),
+		isIPHONE5noMicroMessage = $(window).height()==920 && isiPhone,
+		isIPHONE4noMicroMessage = $(window).height()==744 && isiPhone,
+		isIPHONE5inMicroMessage = $(window).height()==1008 && isiPhone && isMicroMessenger,
+		isIPHONE4inMicroMessage = $(window).height()==832 && isiPhone && isMicroMessenger;
 	$('html').height($(window).height());
 	$('body').height($(window).height());
 	$('body > .sisu').height($(window).height());
-	$(document).on('touchstart','#mainmenu',function(){
+	$(document).on('touchend','#mainmenu',function(evt){
+		var items = $('.share-menu-item.hidden-item');
+		if(items.length > 0)
+			items.removeClass('hidden-item');
+		else
+			$('.share-menu-item').addClass('hidden-item');
+	});
+	$(document).on('touchend','#logo > img',function(evt){
 		var items = $('.share-menu-item.hidden-item');
 		if(items.length > 0)
 			items.removeClass('hidden-item');
@@ -21,7 +34,7 @@ $(function(){
 			if ((curTime - last_update) > 50) {
 				var diffTime = curTime - last_update,
 					OffsetX = acceleration.x * 5;
-				if (iPhoneUA.test(ua)) {
+				if (isiPhone) {
 					OffsetX = -OffsetX;
 				}
 				last_update = curTime;
@@ -33,5 +46,15 @@ $(function(){
 		};
 	if (window.DeviceMotionEvent) {
 		window.addEventListener('devicemotion',deviceMotionHandler, false);  
+    }
+    if(isIPHONE5noMicroMessage || isIPHONE5inMicroMessage) {
+    	$('#weixin').css({top:'-75px'});
+    	$('#renren').css({top:'-170px',left:'160px'});
+    	$('#weibo').css({top:'-240px',left:'145px'});
+    }
+    if(isIPHONE4noMicroMessage || isIPHONE4inMicroMessage) {
+    	$('#weixin').css({top:'-80px'});
+    	$('#renren').css({top:'-180px',left:'170px'});
+    	$('#weibo').css({top:'-255px',left:'165px'});
     }
 });
